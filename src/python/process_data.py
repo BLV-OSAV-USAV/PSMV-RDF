b29850eb3f72c69bf2dfd82e5855e488bf8e286b
GuySchnidrig <g.schnidu@gmail.com>
2026-01-29 13:29:49 +0100

    fix year suffix

--- src/python/process_data.py
+++ src/python/process_data.py
@@ -11,26 +11,26 @@ def process_data():
     data_name = "data/raw/AllProducts280126.csv"
     mapping_table = "data/mapping/mapped_columns.csv"
 
-    # Read data
+    # Read data as df
     df = pd.read_csv(data_name,
         header=0,
         na_values=["NULL"],
         sep=';'
     )
 
-    # Read mapping table
+    # Read mapping table as mapping_df
     mapping_df = pd.read_csv(mapping_table,
         header=0,
         sep=','
     )
 
-    # Map cols
+    # Map cols based on mapping_df 
     df = df.rename(columns=dict(zip(mapping_df.source, mapping_df.target)))
 
     print(df.head(10))
 
     # Save processed file
-    df.to_csv("data/processed/AllProducts280126_mapped.csv", index=False)
+    df.to_csv("data/processed/AllProducts_Renamed.csv", index=False)
 
 
 if __name__ == "__main__":
