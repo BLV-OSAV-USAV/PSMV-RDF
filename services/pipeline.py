66a8926ec4eea96cb9659977c157f5ce291d0cfd
Damian-Oswald <damian.oswald@protonmail.com>
2026-01-29 10:40:34 +0100

    Set up new folder structure (#19)
    - Simpler setup for scripts; only sort by language
    - Orchestrating script in services/pipeline.py
    
    Co-authored-by: GuySchnidrig

---
+++ services/pipeline.py
@@ -0,0 +1,14 @@
+# Run all other scripts
+
+# Syntax validation of all RDF files
+
+# Reason over the graph
+
+# Merge graphs
+
+# a) graph for LINDAS upload
+# b) graph for WevVOWL visualization
+
+# SHACL validation
+
+# Delete & upload named graph on LINDAS
