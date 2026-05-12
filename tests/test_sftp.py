0e45100f2888bb8eb881995819e2e929aac14f49
Guy Schnidrig BLV <78217667+GuySchnidrig@users.noreply.github.com>
2026-05-12 11:02:37 +0200

    Change directory before listing folder contents

--- tests/test_sftp.py
+++ tests/test_sftp.py
@@ -14,4 +14,5 @@ NEEDED_FILES = {
 with FTP(os.environ["SFTP_HOST"]) as ftp:
     ftp.login(user=os.environ["SFTP_USERNAME"], passwd=os.environ["SFTP_PASSWORD"])
     print("Root contents:", ftp.nlst())
+    ftp.cwd("PSM_Verzeichnis")
     print("Folder contents:", ftp.nlst())
