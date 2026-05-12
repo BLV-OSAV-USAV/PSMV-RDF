65b6a353c417512df2e110d76132d749fcdd11bd
Guy Schnidrig BLV <78217667+GuySchnidrig@users.noreply.github.com>
2026-05-12 10:50:20 +0200

    Create test_sftp.py for SFTP file listing
    Add SFTP test script to list required files from FTP server

---
+++ tests/test_sftp.py
@@ -0,0 +1,17 @@
+import os
+from ftplib import FTP
+
+NEEDED_FILES = {
+    "AllProducts.csv", "ApplicationArea.csv", "ApplicationComment.csv",
+    "Code.csv", "IndicationCulture.csv", "IndicationCultureForm.csv",
+    "IndicationMeasure.csv", "IndicationObligation.csv", "IndicationPest.csv",
+    "IndicationTimeMeasure.csv", "ProductCodeR.csv", "ProductCodeS.csv",
+    "ProductDangerSymbol.csv", "ProductFormulationCode.csv", "ProductIngredient.csv",
+    "ProductPermissionHolder.csv", "ProductProductCategory.csv",
+    "ProductSignalWords.csv", "PsmvPermissionholder.csv",
+}
+
+with FTP(os.environ["SFTP_HOST"]) as ftp:
+    ftp.login(user=os.environ["SFTP_USERNAME"], passwd=os.environ["SFTP_PASSWORD"])
+    for filename in sorted(NEEDED_FILES & set(ftp.nlst())):
+        print(filename)
